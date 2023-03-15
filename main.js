11b88976fce0e69af2171ee9a1bac14f28904696
DavidAndrésRuedaChacónCAMPUS <davidarueda18@gmail.com>
2023-03-15 13:37:01 -0500

    feat: :sparkles: Avance en la estructura del programa

--- main.js
+++ main.js
@@ -1,24 +1,31 @@
-let myFormularioCampus = document.querySelector(`#myFormularioCampus`);
+let formularioSede = document.querySelector(`#formularioSede`);
 let myFormularioPersonas = document.querySelector(`#myFormularioPersonas`);
 let campus = {};
 
-myFormularioCampus.addEventListener("submit", (e)=>{
+formularioSede.addEventListener("submit", (e)=>{
     e.preventDefault();
     let data = Object.fromEntries(new FormData(e.target));
     campus[`${data.nombreSede}`] = {Camper: [], trainers: []};
-    listaSedes();
-    myFormularioCampus.reset();
-})
+   /*  listaSedes(); */
+    ciudades();
+    formularioSede.reset();
+    console.log(campus);
+});
 
-let listaSedes = ()=>{
+let ciudades = ()=>{
+    let ubicacion = document.querySelector("[name='ciudad']")
+    let data = 
+}
+
+/* let listaSedes = ()=>{
     let opciones = document.querySelector("[name='sede']")
     opciones.innerHTML = null;
     for (let [val, id] of Object.entries(campus)){
         opciones.insertAdjacentHTML("beforeend", `
-        <option value="${val}">${val}<option/>
+        <option value="${id}">${val}<option/>
         `);       
     };
-}
+} */
 
 myFormularioPersonas.addEventListener("submit", (e)=>{
     e.preventDefault();
