4ef55d81cb11737d0c298ecae7e07a9da7b60e0c
DavidAndrésRuedaChacónCAMPUS <davidarueda18@gmail.com>
2023-03-15 20:14:20 -0500

    feat: :rocket: Avance taller 3
    se logra recolectar información de los campers dependiendo de la sede de campus. Se espera que el código para los trainers sea similar

--- main.js
+++ main.js
@@ -1,42 +1,41 @@
 let formularioSede = document.querySelector(`#formularioSede`);
 let myFormularioPersonas = document.querySelector(`#myFormularioPersonas`);
-let campus = {};
+let infoCampers = document.querySelector(`#infoCampers`);
+const campus = {};
 
 formularioSede.addEventListener("submit", (e)=>{
     e.preventDefault();
     let data = Object.fromEntries(new FormData(e.target));
-    campus[`${data.nombreSede}`] = {Camper: [], trainers: []};
-   /*  listaSedes(); */
-    ciudades();
+    campus[`${data.ciudad}`] = {numero: data.numeroSede ,direccion: data.direccionSede ,Camper: [], trainers: []};
+    listaSedes();
     formularioSede.reset();
-    console.log(campus);
 });
 
-let ciudades = ()=>{
-    let ubicacion = document.querySelector("[name='ciudad']")
-    let data = 
+let listaSedes = ()=>{
+    let sedeSeleccionada = document.querySelector("[name='ciudad']").value;
+    let opciones = document.querySelector("[name='sede']");
+    opciones.innerHTML = null;
+    opciones.insertAdjacentHTML("beforeend", `<option value="${sedeSeleccionada}">${sedeSeleccionada}<option/>"`)    
+
 }
 
-/* let listaSedes = ()=>{
-    let opciones = document.querySelector("[name='sede']")
-    opciones.innerHTML = null;
-    for (let [val, id] of Object.entries(campus)){
-        opciones.insertAdjacentHTML("beforeend", `
-        <option value="${id}">${val}<option/>
-        `);       
-    };
-} */
 
 myFormularioPersonas.addEventListener("submit", (e)=>{
     e.preventDefault();
     let data = Object.fromEntries(new FormData(e.target));
-    console.log(data);
-    let sedes = data.sede;
+    let sede = data.sede;
     delete data.sede;
-
-    campus[`${sedes}`]["Camper"].unshift(data);
+    campus[`${sede}`]["Camper"].unshift(data);
     console.log(campus);
     myFormularioPersonas.reset();
     
 })
 
+/* infoCampers.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    let data = Object.fromEntries(new FormData(e.target));
+    let team = data.team;
+    campus[`${team}`]["Camper"].unshift(data);
+    infoCampers.reset();
+})
+ */
