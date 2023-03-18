e789e999b6597db70f1236ae064cd95ba0db5226
DavidAndrésRuedaChacónCAMPUS <davidarueda18@gmail.com>
2023-03-17 22:28:21 -0500

    feat: :rocket: avance punto 1
    se logró almacenar toda la información con severos problemas
    
    debido a los cambios los datos entre sedes se sobre escriben y no se almacenan correctamente, revisar este tema

--- main.js
+++ main.js
@@ -1,19 +1,11 @@
 let campus = {
-    sede: [/* {
-        numero: "",
-        direccion: "",
-        teams: [{
-            nombreTrainer: [],
-            alumno: [],
-        }]
-    } */]
+    sede: []
 };
 
 let formularioSede = document.querySelector(`#formularioSede`);
 let nivelCamper = document.querySelector(`#nivelCamper`);
 let infoTrainer = document.querySelector(`#infoTrainer`);
 let trainer = document.querySelector(`#Trainer`);
-
 
 formularioSede.addEventListener("submit", (e)=>{
     e.preventDefault();
@@ -48,52 +40,41 @@ trainer.addEventListener("submit", (e)=>{
     listaTrainers();
     let data = Object.fromEntries(new FormData(e.target));
     delete data.sedeT;
-    
-   /*  console.log(data); */
-    /* delete data.teams; */
-/*     let trainerrr = data.nombreTrainer; */
     campus.sede.forEach((item)=>{
+        
         item.teams.forEach((eventos)=>{
-            eventos[data.teams].unshift({
-               data, grupoDelTrainer: []
-            }, 
-            );
-            
-        }) 
-    })
+            if (item.teams[0] == eventos){
+                eventos[data.teams].unshift({
+                   data, grupoDelTrainer: []    
+            });
+        }
+        }); 
+    });
   
     trainer.reset();  
 })
-
 
 nivelCamper.addEventListener("submit", (e)=>{
     e.preventDefault();  
     let data = Object.fromEntries(new FormData(e.target));
     delete data.sedeC;
-    
-    /* console.log("data camper", data); */
     campus.sede.forEach((item)=>{
         item.teams.forEach((eventos)=>{
-       /*      eventos[data.teamsC].unshift(data) */
-            eventos[data.teamsC].forEach((paso)=>{
-                paso.grupoDelTrainer.unshift(data);
-                /* let arrayTrainer = [trainerSeleccionado] */
-              /*   arrayTrainer.unshift(data) */
-              /*   paso.data[nombreTrainer.value].unshift(arrayTrainer) */
-                console.log("aqui pasa algo?", paso.data.nombreTrainer/* [eventos.nombreTrainer] */);
-               /*  paso.nombreTrainer.unshift(data) */
-              /*   paso[eventos.nombreTrainer].unshift(data) */
-             
-            })
-        
+            if (item.teams[0] == eventos){
+                eventos[data.teamsC].forEach((paso)=>{
+                    let salonConfirmacion1 = paso.data.salonTrainer;
+                    let salonConfirmacion2 = data.salonT;
+                    if (salonConfirmacion1 == salonConfirmacion2){
+                        paso.grupoDelTrainer.unshift(data);
+                    }; 
+                });
+            }           
         }) 
+      
     });
-    listaTrainers();
-    console.log(campus);
+    console.log("Objeto campus: ",campus);
     nivelCamper.reset();
 })
-
-
 
 let listaSedes = (p1,p2)=>{
     let sedeSeleccionada = document.querySelector(p1).value;
@@ -117,38 +98,3 @@ let listaTrainers = ()=>{
     trainerrr.insertAdjacentHTML("beforeend", `<option value="${trainerSeleccionado}">${trainerSeleccionado}<option/>"`)
     salonnn.insertAdjacentHTML("beforeend", `<option value="${salonSeleccionado}">${salonSeleccionado}<option/>"`)
 }
-
-
-
-
-
-
-
-/* let formularioSede = document.querySelector(`#formularioSede`);
-let myFormularioPersonas = document.querySelector(`#myFormularioPersonas`);
-let infoCampers = document.querySelector(`#infoCampers`);
-const campus = {};
-formularioSede.addEventListener("submit", (e)=>{
-    e.preventDefault();
-    let data = Object.fromEntries(new FormData(e.target));
-    campus[`${data.ciudad}`] = {numero: data.numeroSede ,direccion: data.direccionSede ,Camper: [], trainers: []};
-    listaSedes();
-    formularioSede.reset();
-});
-let listaSedes = (val, id)=>{
-    let sedeSeleccionada = document.querySelector("[name='ciudad']").value;
-    let opciones = document.querySelector(id);
-    opciones.innerHTML = null;
-    opciones.insertAdjacentHTML("beforeend", `<option value="${sedeSeleccionada}">${sedeSeleccionada}<option/>"`)    
-
-}
-myFormularioPersonas.addEventListener("submit", (e)=>{
-    e.preventDefault();
-    let data = Object.fromEntries(new FormData(e.target));
-    let sede = data.sede;
-    delete data.sede;
-    campus[`${sede}`]["Camper"].unshift(data);
-    console.log(campus);
-    myFormularioPersonas.reset(); 
-}) */
-
